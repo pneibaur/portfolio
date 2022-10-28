228f09f0ef521771f97bd03fc5cf917a6488255d
Phillip Neibaur <pneibaur@gmail.com>
2022-10-28 14:59:38 -0600

    update gatsby-config. rm images src

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
       "name": "projects",
       "path": `${__dirname}/projects`
     },
