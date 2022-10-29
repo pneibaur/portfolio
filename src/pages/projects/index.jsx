31fb15787c11a37eda7d695a85d6ea656bdc1da1
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-10-29 17:26:55 -0600

    adds bootstrap cards to projects index

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,56 +1,76 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { graphql, Link } from 'gatsby'
-import { Row, Col } from "react-bootstrap"
+import { graphql } from 'gatsby'
+import { Row, Col, Card } from "react-bootstrap"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const ProjectsPage = ({ data }) => {
-  let counter = 1
+  let counter = 0
   return (
     <Layout pageTitle="My Projects">
+      <br />
+      {/* maps through the objects */}
       {data.allMdx.nodes.map((node) => {
+        // checks if counter is odd or even. renders different layout depending.
         return counter % 2 === 0 ? (
-          <Row>
-            <Col>
-              <div key={node.id}>
-                <Link to={`/projects/${node.frontmatter.slug}`}>
-                  <h2>Project {node.frontmatter.title}</h2>
-                </Link>
-                <p>Created: {node.frontmatter.date_posted}</p>
-                <p>GitHub: {node.frontmatter.git_repo} </p>
-                <p>Live Link: {node.frontmatter.live_link} </p>
-              </div>
-            </Col>
-            <Col>
-              <GatsbyImage className='projectImage' image={getImage(node.frontmatter.hero_image.childImageSharp)} alt={node.frontmatter.title} />
-              <br /><hr /><br />
-            </Col>
-            <div hidden>{counter = counter + 1}</div>
-          </Row>
+          <>
+            <Row className='justify-content-center'>
+              <Col xs={12} md={4}>
+                <Card className='rCard' key={node.id}>
+                  <Card.Body className='rCardBody'>
+                    <Card.Title as="h1">{node.frontmatter.title}</Card.Title>
+                    <Card.Subtitle className='text-muted'>{node.frontmatter.date_posted}</Card.Subtitle>
+                    <Card.Text className='rCardText' as='h4'>"{node.frontmatter.flair}"</Card.Text>
+                    <hr />
+                    <Card.Text>{node.frontmatter.description}</Card.Text>
+                    <hr />
+                    <Card.Link className='rLink' rel="noreferrer" target='_blank' href={node.frontmatter.git_repo}>
+                      GitHub</Card.Link>
+                    <Card.Link className='rLink' rel="noreferrer" target='_blank' href={node.frontmatter.live_link}>
+                      Live Demo</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col xs={12} md={4}>
+                <GatsbyImage className='projectImage' image={getImage(node.frontmatter.hero_image.childImageSharp)} alt={node.frontmatter.title} />
+              </Col>
+              <div hidden>{counter = counter + 1}</div>
+            </Row>
+            <br /><hr /><br />
+          </>
+          // Ternary operator, for 'else'. renders different layout below.
         ) :
-          <Row>
-            <Col>
-              <GatsbyImage className='projectImage' image={getImage(node.frontmatter.hero_image.childImageSharp)} alt={node.frontmatter.title} />
-              <br /><hr /><br />
-            </Col>
-            <Col>
-              <div key={node.id}>
-                <Link to={`/projects/${node.frontmatter.slug}`}>
-                  <h2>Project {node.frontmatter.title}</h2>
-                </Link>
-                <p>Created: {node.frontmatter.date_posted}</p>
-                <p>GitHub: {node.frontmatter.git_repo} </p>
-                <p>Live Link: {node.frontmatter.live_link} </p>
-              </div>
-            </Col>
-            <div hidden>{counter = counter + 1}</div>
-          </Row>
+          <>
+            <Row className='justify-content-center'>
+              <Col xs={12} md={4}>
+                <GatsbyImage className='projectImage' image={getImage(node.frontmatter.hero_image.childImageSharp)} alt={node.frontmatter.title} />
+              </Col>
+              <Col xs={12} md={4}>
+                <Card className='rCard' key={node.id}>
+                  <Card.Body className='rCardBody'>
+                    <Card.Title as="h1">{node.frontmatter.title}</Card.Title>
+                    <Card.Subtitle className='text-muted'>{node.frontmatter.date_posted}</Card.Subtitle>
+                    <Card.Text className='rCardText' as='h4'>"{node.frontmatter.flair}"</Card.Text>
+                    <hr />
+                    <Card.Text>{node.frontmatter.description}</Card.Text>
+                    <hr />
+                    <Card.Link className='rLink' rel="noreferrer" target='_blank' href={node.frontmatter.git_repo}>
+                      GitHub</Card.Link>
+                    <Card.Link className='rLink' rel="noreferrer" target='_blank' href={node.frontmatter.live_link}>
+                      Live Demo</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <div hidden>{counter = counter + 1}</div>
+            </Row>
+            <br /><hr /><br />
+          </>
       })}
     </Layout>
   )
 }
-
+// data query from graphql
 export const query = graphql`
 query {
     allMdx(sort: {order: DESC, fields: frontmatter___date_posted}) {
@@ -67,6 +87,9 @@ query {
               gatsbyImageData
             }
           }
+          techologies_used
+          description
+          flair
         }
       }
     }
