f39350001335cac076bf2d80cbb4f4fd8b4ea287
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-10-30 16:51:57 -0600

    adds framework logos

--- src/pages/techstack.jsx
+++ src/pages/techstack.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { Row, Col } from 'react-bootstrap'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const TechStack = () => {
     return (
@@ -12,17 +13,37 @@ const TechStack = () => {
             <hr className='h40' />
             <br />
             <Row>
-                <Col xs={12} md={4}>
+                <Col xs={12} md={6}>
                     <h2>Languages</h2>
                     <hr className="h50" />
+                    <StaticImage className='techLogo' src='../images/logos/languages/pythonLogo.png' alt='python-logo' />
+                    <StaticImage className='techLogo' src='../images/logos/languages/htmlLogo.png' alt='html-logo' />
+                    <StaticImage className='techLogo' src='../images/logos/languages/JavaScriptLogo.png' alt='javascript-logo' />
+                    <StaticImage className='techLogo' src='../images/logos/languages/css3Logo.png' alt='css-logo' />
+                    <StaticImage className='techLogo' src='../images/logos/languages/markdownLogo.png' alt='markdown-logo' />
                 </Col>
-                <Col xs={12} md={4}>
+                <Col xs={12} md={6}>
                     <h2>Frameworks</h2>
                     <hr className="h50" />
+                    Materialize, Django, React, express, node, EJS, Gatsby, 
+                    <StaticImage className='techLogo' src='../images/logos/languages/markdownLogo.png' alt='markdown-logo' />
                 </Col>
+            </Row>
+            <Row>
                 <Col xs={12} md={4}>
                     <h2>Libraries</h2>
                     <hr className="h50" />
+                    bootstrap, bulma, skeleton, jquery, 
+                </Col>
+                <Col xs={12} md={4}>
+                    <h2>Databases</h2>
+                    <hr className="h50" />
+                    SQL, MongoDB, Mongoose
+                </Col>
+                <Col xs={12} md={4}>
+                    <h2>Skills</h2>
+                    <hr className="h50" />
+                    Sass, Git & GitHub, Terminal, Firebase, Netlify, Azure, Heroku
                 </Col>
             </Row>
         </Layout>
